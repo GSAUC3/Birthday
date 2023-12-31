ad71166f2e4cde1db9e29717c9ec3736d8ec1c7f
Rajarshi Banerjee <rajarshiban13@gmail.com>
2023-12-31 20:44:57 +0530

    video added

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import './index.scss'
 
 export const Card = () => {
-
-
-
+    const videoRef = useRef(null);
+    const [isHovered, setIsHovered] = useState(false);
+    const [isLoaded, setIsLoaded] = useState(false);
+  
+    useEffect(() => {
+      if (videoRef.current && isHovered && isLoaded) {
+        videoRef.current.play().catch(error => console.error(error));
+      }
+    }, [isHovered, isLoaded]);
+  
+    const handleMouseEnter = () => {
+      setIsHovered(true);
+    };
+  
+    const handleMouseLeave = () => {
+      if (videoRef.current) {
+        videoRef.current.pause();
+      }
+      setIsHovered(false);
+    };
+  
+    const handleVideoLoaded = () => {
+      setIsLoaded(true);
+    };
     return (
         <div className=' relative flex items-center justify-center m-[300px] '>
             <div class="card">
@@ -12,7 +33,8 @@ export const Card = () => {
                     <div class="bark"></div>
                     <img src="https://github.com/GSAUC3/Birthday/blob/main/src/assets/img.jpeg?raw=true" />
                 </div>
-                <div class="details">
+                <div class="details" onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}>
                     <h4 class="color1">Happy Birthday TITHIIIIII</h4>
                     <br />
                     <p>Dear Darling,</p>
@@ -25,9 +47,19 @@ export const Card = () => {
 
                     <p class="text-right">Happy Birthday,Tithi!</p>
                     <p class="text-right">♥Rajarshi</p>
-                <video width="640" height="360" controls>
-                    <source src="tithi/src/assets/wish.mp4" type="video/mp4" />
-                </video>
+                    <div
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}
+                    >
+                        <video
+                            ref={videoRef}
+                            width="640"
+                            height="360"
+                            // controls
+                            onLoadedData={handleVideoLoaded}
+                >                        <source src="https://github.com/GSAUC3/Birthday/blob/main/src/assets/wish.mp4?raw=true" type="video/mp4" />
+                        </video>
+                    </div>
                 </div>
             </div>
         </div>
